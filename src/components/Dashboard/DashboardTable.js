b5b9a54b82fadc33bd19b2d9ab8ffebc24b61da9
Kumar <skk928810@tatatechnologies.com>
2024-04-15 00:49:38 +0530

    enhanced

--- src/components/Dashboard/DashboardTable.js
+++ src/components/Dashboard/DashboardTable.js
@@ -93,7 +93,7 @@ const DashboardTable = () => {
                 <tr key={index}>
                   <td className="px-4 py-2 border border-gray-300">
                     <span
-                      className="text-blue-700 underline cursor-pointer"
+                      className="hover:text-blue-700 hover:underline cursor-pointer"
                       onClick={() => handleIssueClick(item)}
                     >
                       {item.id}
@@ -110,7 +110,7 @@ const DashboardTable = () => {
                   </td>
                   <td className="px-4 py-2 border border-gray-300">
                     <span
-                      className="text-blue-700 underline cursor-pointer"
+                      className="hover:text-blue-700 hover:underline cursor-pointer"
                       onClick={() => handleIssueClick(item)}
                     >
                       {item.issueTitle}
