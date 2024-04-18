da7d76751c3235553f176d463aa6375b2d36218d
FreakWolf <rohitskt115@gmail.com>
2024-04-18 19:14:25 +0530

    created backend for login page

--- src/components/Login/LoginForm.js
+++ src/components/Login/LoginForm.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import { isLength } from "validator";
 import { useNavigate } from "react-router-dom";
-import users from "./user.json";
+// import users from "./user.json";
 
 const LoginForm = () => {
   const [username, setUsername] = useState("");
