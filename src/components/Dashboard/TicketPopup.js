7eefb3616a2c874f26d255f2282993b2df76cabb
Sachin Kumar (TTL) <sk928810.ttl@tatamotors.com>
2024-04-26 14:53:47 +0530

    updated approval page

--- src/components/Dashboard/TicketPopup.js
+++ src/components/Dashboard/TicketPopup.js
@@ -30,25 +30,26 @@ const TicketPopup = ({ ticket, onClose }) => {
     "image",
   ];
 
+
+
   return (
     <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-gray-900 bg-opacity-70">
       <div className="bg-white p-8 rounded-lg relative w-full max-w-3xl h-auto max-h-screen overflow-y-auto">
-        <div className="bg-gray-200 rounded-lg px-4 py-2 mb-4">
-          <div className="flex justify-between items-center">
-            <div>
-              <span className="font-bold">Ticket ID:</span> {ticket.ticket_id}
-            </div>
-            <button
-              className="px-3 py-1 bg-gray-800 text-white rounded-md"
-              onClick={onClose}
-            >
-              X
-            </button>
+        <div className="bg-gray-200 rounded-lg px-4 py-2 mb-4 flex justify-between items-center">
+          <div>
+            <span className="font-bold">Ticket ID:</span> {ticket.ticket_id}
           </div>
+          <button
+            className="px-3 py-1 bg-gray-800 text-white rounded-md"
+            onClick={onClose}
+          >
+            X
+          </button>
         </div>
         <div className="bg-gray-200 rounded-lg px-4 py-2 mb-4">
-          <p className="font-bold">
-            Issue Title:<span>{ticket.issue_title}</span>
+          <p className="font-bold mb-2">
+            Issue Title:
+            <span className="text-gray-600"> {ticket.issue_title}</span>
           </p>
           <p className="mb-2">
             <span className="font-bold">Project:</span> {ticket.project_name}
@@ -58,16 +59,6 @@ const TicketPopup = ({ ticket, onClose }) => {
           </p>
           <p className="mb-2">
             <span className="font-bold">Category:</span> {ticket.category}
-          </p>
-          <p className="mb-0">
-            {new Date(ticket.raised_time).toLocaleString("en-IN", {
-              year: "numeric",
-              month: "numeric",
-              day: "numeric",
-              hour: "2-digit",
-              minute: "2-digit",
-              hour12: true,
-            })}
           </p>
         </div>
 
@@ -83,11 +74,11 @@ const TicketPopup = ({ ticket, onClose }) => {
             onChange={setDescription}
             modules={modules}
             formats={formats}
-            className="bg-white"
+            className="bg-white shadow-lg"
           />
         </div>
 
-        <div className="flex justify-end">
+        <div className="flex justify-between">
           <button className="px-6 py-2 bg-green-500 text-white rounded-md mr-4">
             Approve
           </button>
